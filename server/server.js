4512348f6fbbe39a303a6e509a86f519938f4a08
G40rc3 <G40rc3@gmail.com>
2025-03-28 12:40:55 +0000

    added server get and post to client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const cors = require("cors");
+const { configDotenv } = require("dotenv");
 const express = require("express");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST"],
+  })
+);
+configDotenv.config();
+
+app.get("/", function (request, response) {
+  response.json({ message: "Beep" });
+});
+
+app.post("/message", function (request, response) {
+  response.json({ message: "Boop" });
+});
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
