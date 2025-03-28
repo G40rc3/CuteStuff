68a39146724af8abe8545f78d2f1859b4895e4eb
G40rc3 <G40rc3@gmail.com>
2025-03-28 12:58:46 +0000

    redoing the server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const cors = require("cors");
-const { configDotenv } = require("dotenv");
-const express = require("express");
-const app = express();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(
