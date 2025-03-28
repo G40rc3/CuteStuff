82dc2ecfa0c8935620436cbe4745e409a81513ea
G40rc3 <G40rc3@gmail.com>
2025-03-28 12:36:13 +0000

    recommit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
