fbbb9857852f9a60a31bc728f2d072f55e202a4a
G40rc3 <G40rc3@gmail.com>
2025-03-28 13:19:26 +0000

    trial and error

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-
+app.use(express.json());
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST"],
+  })
+);
 dotenv.config();
 
 app.get("/", function (request, response) {
@@ -14,7 +20,7 @@ app.post("/message", function (request, response) {
   response.json({ message: "Boop" });
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log(`Server is listening on port ${port}`);
 });
